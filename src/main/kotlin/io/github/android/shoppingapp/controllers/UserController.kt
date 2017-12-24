5e4b1157b673284e4c748828ecc4fe6567b14285
Abhinav <abhinav.manchanda@gmail.com>
2017-12-24 21:15:15 +0530

    Removed unused controllers.

--- src/main/kotlin/io/github/android/shoppingapp/controllers/UserController.kt
+++
@@ -1,9 +0,0 @@
-package io.github.android.shoppingapp.controllers
-
-import org.springframework.data.rest.webmvc.RepositoryRestController
-import org.springframework.web.bind.annotation.RequestMapping
-
-//@RepositoryRestController
-//@RequestMapping("/users")
-class UserController {
-}
